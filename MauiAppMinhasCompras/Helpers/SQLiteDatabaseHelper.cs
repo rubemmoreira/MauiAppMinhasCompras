2da3e770215be0ed8b78b730508c3aff070f5ad6
Rubem Moreira <rubemmoreira.rm@gmail.com>
2025-09-14 09:26:02 -0300

    definição de uma classe utilitária, o SQliteDatabaseHelper. E essa classe serve para gerenciar o CRUD que afremos do Banco

--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using MauiAppMinhasCompras.Models;
+using SQLite;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MauiAppMinhasCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
+        private readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        // Inserir um novo produto
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        // Atualizar um produto existente
+        public Task<int> Update(Produto p)
+        {
+            return _conn.UpdateAsync(p);
+        }
+
+        // Deletar um produto pelo ID
+        public Task<int> Delete(int id)
+        {
+            return _conn.DeleteAsync<Produto>(id);
+        }
+
+        // Buscar todos os produtos
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        // Buscar produtos por parte da descrição
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE ?";
+            return _conn.QueryAsync<Produto>(sql, $"%{q}%");
+        }
     }
 }
